fd41716813ab3a64cac777eec7139a917d4fabcf
johnson_phillips <johnson.phillips@mheducation.com>
2020-06-24 19:03:12 -0400

    updated version

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import {TestData} from "./testdata";
 import logger from "./logger";
 
 
-global.report = {
+const report = {
     testData: new TestData(),
     logger: logger
 };
+
+
+export default report;
--- test/spec.ts
+++ test/spec.ts
@@ -1,20 +1,14 @@
-import {TestData} from "../src/testdata";
-import logger from "../src/logger";
+import report from '../src/main';
 
 async function test() {
-
-    global.report = {
-        testData: new TestData(),
-        logger: logger
-    };
-    global.report.testData.deleteReportFolder();
-    global.report.testData.startTest('report library features','test to show report library features');
-    await global.report.testData.addTestStep('this is addTestStep method',null);
-    await global.report.testData.addTestStep('this is addTestStep with screenshot',null,'imagename');
-    await global.report.testData.addTestStep('this is addTestStep with api true',null,true);
-    await global.report.testData.addAssertStep('verify abc = ABC','abc','ABC');
-    await global.report.testData.addAssertStep('verify abc = abc','abc','abc');
-    await global.report.testData.endTest();
+    report.testData.deleteReportFolder();
+    report.testData.startTest('report library features','test to show report library features');
+    await report.testData.addTestStep('this is addTestStep method',null);
+    await report.testData.addTestStep('this is addTestStep with screenshot',null,'imagename');
+    await report.testData.addTestStep('this is addTestStep with api true',null,true);
+    await report.testData.addAssertStep('verify abc = ABC','abc','ABC');
+    await report.testData.addAssertStep('verify abc = abc','abc','abc');
+    await report.testData.endTest();
 }
 
 ( async () => {
