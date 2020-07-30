21b2a59e1255493115b0eaed4dddd192175dda51
johnson_phillips <johnson.phillips@mheducation.com>
2020-07-30 16:21:22 -0400

    fixed import issue

--- src/testData.ts
+++ src/testData.ts
@@ -4,7 +4,7 @@ import Suite from './suite';
 let path = require('path');
 let fs = require('fs');
 import {strict as assert} from "assert";
-import htmlReport from './htmlreport';
+import htmlReport from './htmlReport';
 import logger from "./logger";
 const fsExtra = require('fs-extra');
 
